3685e03e65d89106e1ef5418ab38e6311f3b095a
inno-cent <ekweozorinnocent@gmail.com>
2024-09-03 20:21:28 +0100

    login page

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 
-
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigation = useNavigation();
   const router = useRouter();
 
-
   const login = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
+    console.log("Email:", email);
+    console.log("Password:", password);
     // Handle login logic here
   };
 
@@ -22,24 +28,26 @@ const LoginScreen = () => {
       <View style={styles.formSection}>
         <View style={styles.formHeadText}>
           <Text style={styles.header}>Welcome Back!</Text>
-          <Text style={styles.subText}>Please enter your details to login.</Text>
+          <Text style={styles.subText}>Login to your account.</Text>
         </View>
         <View style={styles.inputGroup}>
-        <Text style={styles.label}>Email</Text>
+          <Text style={styles.label}>Email</Text>
           <TextInput
             style={styles.input}
-            placeholder="Email"
+            placeholder="PeachAlda@gmail.com"
             value={email}
+            placeholderTextColor="#1B263BE5" 
             onChangeText={setEmail}
           />
         </View>
         <View style={styles.inputGroup}>
-        <Text style={styles.label}>Password</Text>
+          <Text style={styles.label}>Password</Text>
           <TextInput
             style={styles.input}
-            placeholder="Password"
+            placeholder="........"
             secureTextEntry
             value={password}
+            placeholderTextColor="#1B263BE5" 
             onChangeText={setPassword}
           />
         </View>
@@ -64,7 +72,10 @@ const LoginScreen = () => {
         </View>
         <TouchableOpacity style={styles.googleSignupWrapper}>
           <View style={styles.innerContainer}>
-            <Image source={require('../../assets/images/Google.png')} style={styles.logo} />
+            <Image
+              source={require("../../assets/images/Google.png")}
+              style={styles.logo}
+            />
             <Text style={styles.googleText}>Continue with Google</Text>
           </View>
         </TouchableOpacity>
@@ -84,111 +95,112 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   loginContainer: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center",
     paddingHorizontal: 20,
-    paddingVertical: 80, 
-    fontFamily: 'Inter', 
+    paddingVertical: 80,
   },
   formSection: {
-    width: '100%',
+    width: "100%",
     maxWidth: 400,
     padding: 20,
   },
   formHeadText: {
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
   header: {
     fontSize: 25,
-    fontWeight: '700',
-    color: '#1b263b',
+    fontWeight: "700",
+    color: "#1b263b",
+    textAlign: "center",
   },
   subText: {
     fontSize: 18,
-    fontWeight: '400',
-    color: '#1b263b',
+    fontWeight: "400",
+    color: "#1b263b",
     marginBottom: 20,
+    textAlign: "center",
+    lineHeight: 27,
   },
   inputGroup: {
     marginBottom: 15,
   },
   label: {
     fontSize: 14,
-    fontWeight: '400',
+    fontWeight: "400",
     marginBottom: 5,
-    color: '#1B263B',
-
+    color: "#1B263B",
   },
   input: {
     height: 44,
     padding: 12,
     fontSize: 14,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 4,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   bottomTextContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 25,
   },
   rememberTextInner: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   checkboxWrapper: {
     marginRight: 10,
   },
   rememberText: {
     fontSize: 13,
-    fontWeight: '400',
-    color: '#1b263b',
+    fontWeight: "400",
+    color: "#1b263b",
   },
   forgetPassword: {
     fontSize: 13,
-    fontWeight: '400',
-    color: '#778da9',
+    fontWeight: "400",
+    color: "#778da9",
   },
   formSubmitButton: {
-    backgroundColor: '#415a77',
+    backgroundColor: "#415a77",
     padding: 12,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 25,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 15,
   },
   divider: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 30,
   },
   dividerLine: {
     flex: 1,
     height: 1,
-    backgroundColor: '#778da980',
+    backgroundColor: "#778da980",
   },
   dividerText: {
     marginHorizontal: 10,
     fontSize: 16,
-    color: '#333',
+    color: "#333",
   },
   googleSignupWrapper: {
-    borderColor: '#778da980',
+    borderColor: "#778da980",
     borderWidth: 1,
     borderRadius: 10,
     padding: 15,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 40,
   },
   innerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   logo: {
     width: 24,
@@ -197,17 +209,17 @@ const styles = StyleSheet.create({
   },
   googleText: {
     fontSize: 15,
-    color: '#1b263b',
+    color: "#1b263b",
   },
   acctTextContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   acctText: {
     fontSize: 15,
-    color: '#778da9',
+    color: "#778da9",
   },
   createAccountText: {
-    color: '#415a77',
+    color: "#415a77",
   },
 });
 
