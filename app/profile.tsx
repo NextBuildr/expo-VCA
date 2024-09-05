9a0c253f58a453ac8fd02f47f1686b3e28adab2c
inno-cent <ekweozorinnocent@gmail.com>
2024-09-05 15:22:58 +0100

    feat: profile page

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons, Entypo } from "@expo/vector-icons";
 import NavigationTab from "../components/navigation-tab";
 
 const ProfilePage = () => {
-    const [activeTab, setActiveTab] = useState("call");
-    const handleTabPress = (tab: React.SetStateAction<string>) => {
-        setActiveTab(tab);
-      };
+  const [activeTab, setActiveTab] = useState("call");
+  const handleTabPress = (tab: React.SetStateAction<string>) => {
+    setActiveTab(tab);
+  };
   return (
     <View style={styles.container}>
       {/* Profile Picture */}
@@ -22,37 +22,54 @@ const ProfilePage = () => {
       <Text style={styles.username}>@johndoe</Text>
 
       <TouchableOpacity style={styles.formSubmitButton}>
-      <Feather name="edit-2" size={20} color="#FFF" style={styles.icon} />
-      <Text style={styles.buttonText}>Edit Profile</Text>
-    </TouchableOpacity>
+        <Feather name="edit-2" size={20} color="#FFF" style={styles.icon} />
+        <Text style={styles.buttonText}>Edit Profile</Text>
+      </TouchableOpacity>
       {/* Horizontal Line */}
       <View style={styles.horizontalLine} />
 
-      {/* List of Options */}
-      <View style={styles.optionsContainer}>
-        {[
-          { label: "Settings", icon: "cog" },
-          { label: "Notification", icon: "bell" },
-          { label: "Change Password", icon: "lock" },
-        ].map((item, index) => (
-          <TouchableOpacity key={index} style={styles.optionItem}>
-            <Text style={styles.optionText}>{item.label}</Text>
-            <FontAwesome name="chevron-right" size={18} color="#415A77" />
-          </TouchableOpacity>
-        ))}
+      {/* Settings List */}
+      <View style={styles.settingsList}>
+        <TouchableOpacity style={styles.listItem}>
+          <View style={styles.listItemContent}>
+            <FontAwesome name="cog" size={24} color="#1B263B" />
+            <Text style={styles.listItemText}>Settings</Text>
+          </View>
+          <Entypo name="chevron-right" size={24} color="#778DA9" />
+        </TouchableOpacity>
 
-        {/* Horizontal Line after Change Password */}
+        <TouchableOpacity style={styles.listItem}>
+          <View style={styles.listItemContent}>
+            <MaterialIcons name="notifications" size={24} color="#1B263B" />
+            <Text style={styles.listItemText}>Notification</Text>
+          </View>
+          <Entypo name="chevron-right" size={24} color="#778DA9" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.listItem}>
+          <View style={styles.listItemContent}>
+            <FontAwesome name="lock" size={24} color="#1B263B" />
+            <Text style={styles.listItemText}>Change Password</Text>
+          </View>
+          <Entypo name="chevron-right" size={24} color="#778DA9" />
+        </TouchableOpacity>
         <View style={styles.horizontalLine} />
 
-        {[
-          { label: "Help and Support", icon: "question-circle" },
-          { label: "Log Out", icon: "sign-out" },
-        ].map((item, index) => (
-          <TouchableOpacity key={index} style={styles.optionItem}>
-            <Text style={styles.optionText}>{item.label}</Text>
-            <FontAwesome name="chevron-right" size={18} color="#415A77" />
-          </TouchableOpacity>
-        ))}
+        <TouchableOpacity style={styles.listItem}>
+          <View style={styles.listItemContent}>
+            <MaterialIcons name="help-outline" size={24} color="#1B263B" />
+            <Text style={styles.listItemText}>Help & Support</Text>
+          </View>
+          <Entypo name="chevron-right" size={24} color="#778DA9" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.listItem}>
+          <View style={styles.listItemContent}>
+            <FontAwesome name="sign-out" size={24} color="#1B263B" />
+            <Text style={styles.listItemText}>Log Out</Text>
+          </View>
+          <Entypo name="chevron-right" size={24} color="#778DA9" />
+        </TouchableOpacity>
       </View>
       <NavigationTab activeTab={activeTab} handleTabPress={handleTabPress} />
     </View>
@@ -90,21 +107,13 @@ const styles = StyleSheet.create({
     borderBottomColor: "#D9D9D9",
     marginVertical: 20,
   },
-  optionsContainer: {
-    width: "100%",
-  },
+
   icon: {
-    marginRight: 10, 
+    marginRight: 10,
   },
-  optionItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 15,
-    // borderBottomWidth: 1,
-    // borderBottomColor: "#D9D9D9",
-  },
+
   formSubmitButton: {
+    flexDirection: "row",
     backgroundColor: "#415a77",
     padding: 12,
     borderRadius: 10,
@@ -116,6 +125,27 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
   optionText: {
+    fontSize: 18,
+    color: "#1B263B",
+  },
+  settingsList: {
+    marginTop: 10,
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 15,
+    width: "100%",
+    borderBottomColor: "#D9D9D9",
+    // borderBottomWidth: 1,
+  },
+  listItemContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  listItemText: {
+    marginLeft: 10,
     fontSize: 18,
     color: "#1B263B",
   },
