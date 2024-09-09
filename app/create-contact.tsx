bf2c8c31b02a61a75668e1891f68015bed246bf1
inno-cent <ekweozorinnocent@gmail.com>
2024-09-09 09:31:45 +0100

    edit-profile fix

--- app/create-contact.tsx
+++ app/create-contact.tsx
@@ -61,36 +61,31 @@ const createContact = ({}) => {
             />
           </View>
           <View style={styles.inputWrapperFlex}>
-  {/* Country Input */}
-  <View style={styles.inputContainer}>
-    <Text style={styles.label}>Country</Text> {/* Label on top */}
-    <View style={styles.inputWrapper}>
-      <AntDesign
-        name="phone"
-        size={20}
-        color="#1B263B"
-        style={styles.icon}
-      />
-      <TextInput
-        placeholder="NG"
-        placeholderTextColor="#1B263BE5"
-        style={styles.inputWithIcon}
-      />
-    </View>
-  </View>
+            <View style={styles.inputWrapper}>
+              <AntDesign
+                name="phone"
+                size={20}
+                color="#1B263B"
+                style={styles.icon}
+              />
+              <View style={styles.inputInnerWrapper}>
+                <Text style={styles.label}>Full Name</Text>
+                <TextInput
+                  placeholder="First name"
+                  placeholderTextColor="#1B263BE5"
+                  style={styles.inputWithIcon}
+                />
+              </View>
+            </View>
 
-  {/* Phone Input */}
-  <View style={styles.inputContainerNoLabel}>
-    <View style={styles.inputWrapper}>
-      <TextInput
-        placeholder="Phone"
-        placeholderTextColor="#1B263BE5"
-        style={styles.inputWithIcon}
-        keyboardType="numeric"
-      />
-    </View>
-  </View>
-</View>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                placeholder="Phone"
+                placeholderTextColor="#1B263BE5"
+                style={styles.inputWithIcon}
+              />
+            </View>
+          </View>
         </View>
 
         <TouchableOpacity
@@ -146,6 +141,7 @@ const styles = StyleSheet.create({
     // marginBottom: 5,
     lineHeight: 27,
     color: "#1B263B",
+    paddingLeft: 12,
   },
   emptyIcon: {
     width: 30, // This should match the icon size for alignment
